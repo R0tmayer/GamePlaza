2168194047f245ba25bdd33cdf0032364ff2335c
Сальников Роман <roman.salnikov@deltaleasing.ru>
2024-07-18 18:09:55 +1000

    Add login and register page

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
 	<Navbar></Navbar>
-	<div class="container mx-auto mt-5">
+	<div class="container mx-auto mt-5 flex">
 		<!-- <ProductList></ProductList> -->
-		<ProductOverview></ProductOverview>
+		<!-- <ProductOverview></ProductOverview> -->
+		<Register></Register>
+		<Login></Login>
 	</div>
 </template>
 
 <script setup lang="ts">
 import Navbar from './components/custom/Navbar.vue';
+import Login from './components/pages/Login.vue';
 import ProductList from './components/pages/ProductList.vue';
 import ProductOverview from './components/pages/ProductOverview.vue';
+import Register from './components/pages/Register.vue';
 </script>
