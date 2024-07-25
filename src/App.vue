af711b429c3c63a0be11ab89673bf4d09cb0f227
rotmayer <r.salnikov1998@gmail.com>
2024-07-26 08:35:43 +1000

    backup

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,14 @@
 		<!-- <ProductOverview></ProductOverview> -->
 		<!-- <Register></Register> -->
 		<!-- <Login></Login> -->
-		<ShoppingCart></ShoppingCart>
+		<!-- <ShoppingCart></ShoppingCart> -->
+		<DashboardOrders></DashboardOrders>
 	</div>
 </template>
 
 <script setup lang="ts">
 import Navbar from './components/custom/Navbar.vue';
+import DashboardOrders from './components/pages/DashboardOrders..vue';
 import Login from './components/pages/Login.vue';
 import ProductList from './components/pages/ProductList.vue';
 import ProductOverview from './components/pages/ProductOverview.vue';
